a5fa88b949797c7203c49b79438f5e08fbefa587
jhasan018 <jahid.bubtcse29@gmail.com>
2022-11-13 22:05:00 +0600

    Product Search

---
+++ app.js
@@ -0,0 +1,20 @@
+const search = () => {
+    const searchBox = document.getElementById("search-item").value.toUpperCase();
+    const storeItems = document.getElementById("product-list");
+    const product = document.querySelectorAll(".product");
+    const productName = document.getElementsByTagName("h3")
+
+    for( let i=0; i< productName.length; i++){
+        let match = product[i].getElementsByTagName("h3")[0];
+
+        if(match){
+            let textValue = match.textContent || match.innerHTML;
+
+            if(textValue.toUpperCase().indexOf(searchBox) > -1){
+                product[i].style.display = "";
+            }else{
+                product[i].style.display = "none";
+            }
+        }
+    }
+}
